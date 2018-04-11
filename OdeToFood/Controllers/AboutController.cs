44b7d9a21d2df961afbf79e8b0185905eebb1ed2
Kayden Miller <kaydenmiller1@gmail.com>
2018-04-10 23:18:58 -0600

    Explores Convention Based Routing

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "1+ 801-388-5765";
+        }
+
+        public string Address()
+        {
+            return "2520 W. 1675 S.";
+        }
+    }
+}
