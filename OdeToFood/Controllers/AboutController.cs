cb2ad32fc2d733a9aa4b70586e44f03048ac62af
Kayden Miller <kaydenmiller1@gmail.com>
2018-04-10 23:23:54 -0600

    Explores attribute based routing

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    // About
+
+    [Route("[controller]/[action]")]
     public class AboutController
     {
+        //[Route("")]
         public string Phone()
         {
             return "1+ 801-388-5765";
         }
 
+        //[Route("[action]")]
         public string Address()
         {
             return "2520 W. 1675 S.";
